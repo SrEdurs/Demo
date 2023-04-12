09c111192c849ba69e90a65fa7e307dcd5159e45
Eduardo Rojas <edu_mairenero@hotmail.com>
2023-04-12 18:53:31 +0200

    first commit

---
+++ demo/src/main/java/com/example/demo/service/AbstractBusinessService.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.service.mapper.AbstractServiceMapper;
+import java.util.*;
+
+public abstract class AbstractBusinessService <E, ID, DTO,  REPO extends JpaRepository<E,Long> ,
+        MAPPER extends AbstractServiceMapper<E,DTO>> {
+    private final REPO repo;
+    private final MAPPER serviceMapper;
+
+
+    protected AbstractBusinessService(REPO repo, MAPPER mapper) {
+        this.repo = repo;
+        this.serviceMapper = mapper;
+    }
+    //Lista de todos los DTOs buscarTodos devolvera lista
+    public List<DTO> buscarTodos(){
+        return  this.serviceMapper.toDto(this.repo.findAll());
+    }
+
+    public List<E> buscarEntidades(){
+        return  this.repo.findAll();
+    }
+
+    //Buscar por id
+    public Optional<DTO> encuentraPorId(long l){
+
+        return this.repo.findById(l).map(this.serviceMapper::toDto);
+    }
+    public Optional<E> encuentraPorIdEntity(long id){
+
+        return this.repo.findById(id);
+    }
+    //Guardar
+    public DTO guardar(DTO dto) throws Exception {
+        //Traduzco del dto con datos de entrada a la entidad
+        final E entidad = serviceMapper.toEntity(dto);
+        //Guardo el la base de datos
+        E entidadGuardada =  repo.save(entidad);
+        //Traducir la entidad a DTO para devolver el DTO
+        return serviceMapper.toDto(entidadGuardada);
+    }
+
+    //Obtener el mapper
+    public MAPPER getMapper(){return  serviceMapper;}
+    //Obtener el repo
+    public REPO getRepo(){return  repo;}
+}
---
+++ demo/src/main/java/com/example/demo/service/mapper/AbstractServiceMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.service.mapper;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public abstract class AbstractServiceMapper<E,DTO> {
+    //Convertir de entidad a dto
+    public abstract DTO toDto(E e);
+    //Convertir de dto a entidad
+    public abstract E toEntity(DTO dto) throws Exception;
+    //Conversion de listas de entidades a DTOs
+    public List<DTO>  toDto(List<E> e){
+        return e.stream().map(this::toDto).collect(Collectors.toList());
+    }
+}
